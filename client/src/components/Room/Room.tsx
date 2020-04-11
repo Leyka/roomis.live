4b46c17b56127def077c4de17e6a9e991ca70331
Skander <skander.kc@gmail.com>
2020-04-11 00:23:40 -0400

    CSS fix Room

--- client/src/components/Room/Room.tsx
+++ client/src/components/Room/Room.tsx
@@ -3,14 +3,13 @@ import { socket } from '@/utils/socket';
 import { RoomEvent } from '@shared/events';
 import { kebabCase } from 'lodash';
 import { useObserver } from 'mobx-react-lite';
-import React, { FC, useEffect, useState } from 'react';
+import React, { FC, useEffect } from 'react';
 import { useParams } from 'react-router';
 import { LivePlayer } from '../LivePlayer/LivePlayer';
 import { Logger } from '../Logger/Logger';
 import { RoomLayout } from './RoomLayout';
 
 export const Room: FC = () => {
-  const [canEdit, setCanEdit] = useState(false); // TODO: Delete
   const { roomStore, userStore } = useRootStore();
 
   // Returns name of the room that users entered in URL bar
@@ -24,12 +23,7 @@ export const Room: FC = () => {
     return () => {
       socket.off(RoomEvent.UserDisconnect);
     };
-  }, []);
-
-  const onGuestsCanEditClick = () => {
-    setCanEdit(!canEdit);
-    socket!.emit(RoomEvent.GuestsCanEdit, { canEdit: !canEdit });
-  };
+  }, [roomStore.roomName, userStore]);
 
   return useObserver(() => {
     if (!socket) {
@@ -38,15 +32,7 @@ export const Room: FC = () => {
 
     return (
       <RoomLayout
-        header={
-          <div>
-            Room: {roomStore.roomName}
-            <div>
-              Test only: <button onClick={onGuestsCanEditClick}>Toggle guest right</button>
-            </div>
-            <div>Can Edit ? {userStore.canEdit ? 'Yes' : 'No'}</div>
-          </div>
-        }
+        header={<div>Room: {roomStore.roomName}</div>}
         playlist={<div>Playlist</div>}
         player={<LivePlayer roomName={roomStore.roomName} userCanEdit userIsHost />}
         logger={<Logger />}
